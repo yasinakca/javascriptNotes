bdc9f93ca7ea6f776b73ec4f372dcb36480536bf
yasinakca <yasinakcadev@gmail.com>
2021-12-22 14:46:16 +0300

    loops

--- notlar.js
+++ notlar.js
@@ -88,6 +88,8 @@
 
     let array = []  //bos bir array yaratir
 
+    Array, referans tipli bir veri tipidir.     
+
     #Array Methodları
 
     - push => Sona bir eleman ekler
@@ -102,6 +104,65 @@
     yeni bir array olarak doner. Mevcut array uzerinde degisiklik yapmaz.
     - splice(start,?deleteCount,itemss) => array icindeki mevcut ogeleri kaldırarak veya 
     degistirerek ve/veya uzerine yeni ogeler ekleyerek arrayin icerigini degistirir.
+
+    Arrayler reference type bir veri tipi oldugu icin const ile bir array tanimlamasi
+    yaptigimiz zaman push pop gibi islemler hata vermeyecektir. array e yeni bir deger
+    atamasi yapmadigimiz surece arrayin referansını degil sadece icerigini degistiririz
+    bu da bir problem yaratmayacaktır.
+
+    #Objects
+
+    Js' de objeler, propertylerin bir araya gelmesiyle oluşan koleksiyonlardır.
+    Burada property'ler key-value ikililerinden oluşan yapılardır. Array veri yapısının 
+    aksine elemanlara indexleri ile erismektense objelerde propertyler uzerinden
+    erisiriz. 
+
+    Objelerin propertylerine dot notasyon ile erisiriz
+
+        const user = {
+            "nickname" : "deniz",
+            "age" : 23,
+        }
+
+        user.nickname => "deniz"
+
+    Objenin propertysine erismek icin arraylerde oldugu gibi [] de kullanılabilir.
+
+        user["age"] => 23
+
+    Objeye bir property daha eklemek icin dot notasyon veya [] kullanılabilir.
+
+        user.province = "Istanbul"
+
+        user["district"] = "Orta"
+
+    #Donguler
+
+    Kod bloklarını tekrarlamaya yarayan yapilar
+
+    For dongusu
+        
+        for(let i=0; i<10; i++) {
+            console.log("Hello" + i)
+        }
+
+        let array = [1,2,3,"a","b","c"]
+
+        for(let i=0; i<array.length;i++) {
+            console.log(array[i]);
+        }
+
+    While dongusu
+
+        let i = 10;
+
+        while(i>=0) {
+            i--;
+            console.log("down...")
+        }
+
+
+
 
     
 */
